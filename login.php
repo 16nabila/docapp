7c2ed14dbfd8f19641b804c19ac13535256b5b3f
16nabila <nabilasarkar169148@gmail.com>
2024-12-03 12:01:01 +0600

    password issue fixed

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = MD5('$password')";
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
